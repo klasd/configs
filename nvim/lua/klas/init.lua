0466a5d9ca0b7f0437e626cb3792044ff5821e33
Klas Dahlen <klasdahlen@pop-os.localdomain>
2023-06-03 19:03:09 +0200

    updated config

--- nvim/lua/klas/init.lua
+++ nvim/lua/klas/init.lua
@@ -1,7 +1,61 @@
 require("klas.packer")
 require("klas.remap")
 -- print("hello from klas")
+
+-- relative numbers 
 vim.wo.number = true
 vim.o.relativenumber = true
 
+-- tab = 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
 require("nvim-tree").setup()
+
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = { "node_modules", "bin", "obj", "target", ".git" }
+  }
+}
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
--- nvim/lua/klas/remap.lua
+++ nvim/lua/klas/remap.lua
@@ -5,3 +5,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ft", ":NvimTreeToggle<cr>")
 vim.keymap.set("n", "<leader>ff", ":NvimTreeFocus<cr>")
 vim.keymap.set("n", "<leader>fl", ":NvimTreeFindFile<cr>")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")

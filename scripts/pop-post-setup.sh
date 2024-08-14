dbddc9b2bce48c89d62c12bb12180aa5f44bd51e
Klas Dahlén <6842922+klasd@users.noreply.github.com>
2024-08-14 14:42:49 +0200

    .ideavimrc

--- scripts/pop-post-setup.sh
+++ scripts/pop-post-setup.sh
@@ -1,3 +1,8 @@
+## Install PPA-apps
+sudo apt-get install -y alacritty tmux gcc g++ make cmake git curl wget htop \
+  neofetch zsh fonts-firacode fonts-powerline fonts-font-awesome \
+  xclip xsel xdotool x11-xserver-utils x11-utils x11-xserver-utils
+
 ## Install .NET SDK 8.0
 sudo apt-get update &&
   sudo apt-get install -y dotnet-sdk-8.0
@@ -5,7 +10,7 @@ sudo apt-get update &&
 ## Install neovim
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt-get update
-sudo apt install neovim -y
+sudo apt-get install neovim -y
 
 ## Install regolith desktop
 wget -qO - https://regolith-desktop.org/regolith.key |
@@ -21,3 +26,6 @@ sudo apt install regolith-desktop regolith-session-flashback regolith-look-lasca
 ## Download Jetbrains Toolbox
 wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.4.1.32573.tar.gz ~/Downloads/
 tar xvzf ~/Downloads/jetbrains-toolbox-2.4.1.32573.tar.gz ~/Downloads/
+
+##Install Rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
